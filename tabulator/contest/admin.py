2b3924245feb9f90cd86c3ed094f6917f4942c01
azuer88 <blue.cuenca@gmail.com>
2016-01-12 14:30:30 +0800

    complete contest data definitions

--- tabulator/contest/admin.py
+++ tabulator/contest/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from models import Category, Criterion, Group, Candidate, Score
+
 # Register your models here.
+admin.site.register(Candidate)
+admin.site.register(Criterion)
+admin.site.register(Category)
+admin.site.register(Group)
+admin.site.register(Score)
--- tabulator/contest/models.py
+++ tabulator/contest/models.py
@@ -16,6 +16,8 @@ class Judge(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=60)
 
+    def __unicode__(self):
+        return u"{}".format(self.name)
 
 class Category(models.Model):
     name = models.CharField(max_length=60)
@@ -23,32 +25,54 @@ class Category(models.Model):
     sequence = models.IntegerField()
     weight = weight_field()
 
+    def __unicode__(self):
+        return u"{}".format(self.name)
+
+    class Meta:
+        ordering = ['sequence']
+
 
 class Criterion(models.Model):
     short_name = models.CharField(max_length=10)
     name = models.CharField(max_length=60)
     description = models.CharField(max_length=200, blank=True, default='')
     sequence = models.IntegerField()
-    catgegory = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     weight = weight_field()
 
+    def __unicode__(self):
+        return u"{}".format(self.name)
+
     class Meta:
-        ordering = ['sequence']
-        order_with_respect_to = 'category'
+        ordering = ['category', 'sequence']
 
 
 class Group(models.Model):
     name = models.CharField(max_length=60)
 
+    def __unicode__(self):
+        return u"{}".format(self.name)
 
-class Candidate(models.Models):
+
+class Candidate(models.Model):
     name = models.CharField(max_length=60)
     number = models.IntegerField()
     group = models.ForeignKey(Group, on_delete=models.CASCADE)
+
+    def __unicode__(self):
+        return u"{}".format(self.name)
 
     class Meta:
         ordering = ['number']
 
 
-class Score(models.models):
-    pass
+class Score(models.Model):
+    candidate = models.ForeignKey(Candidate, on_delete=models.CASCADE)
+    criterion = models.ForeignKey(Criterion, on_delete=models.CASCADE)
+    score = models.IntegerField()
+
+    def __unicode__(self):
+        return u"{}".format(self.score)
+
+    class Meta:
+        unique_together = [('candidate','criterion'),]
