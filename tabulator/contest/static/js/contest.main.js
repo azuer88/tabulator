e789da24960d1bf356307663832ce3c1a8c22d8c
Blue Cuenca <blue.cuenca@gmail.com>
2016-01-28 04:24:28 +0800

    attach event handlers for on change for all dynamically loaded (input) html

--- tabulator/contest/static/js/contest.main.js
+++ tabulator/contest/static/js/contest.main.js
@@ -1,17 +1,17 @@
+function postScore(c, target_url) {
+   var input_name = c.attr("name");
+   var input_value = c.attr("value");
+   var input_data = {name: input_name, value: input_value};
+  
+   $.ajax({
+       type: "POST",
+       url: target_url,
+       data: input_data.serialize(),
+       success: function(){}
+   })
+};
+   
 $(document).ready(function() {
-    formmodified=0;
-    $('form *').change(function(){
-        formmodified=1;
-    });
-    window.onbeforeunload = confirmExit;
-    function confirmExit() {
-        if (formmodified == 1) {
-            return "New information not saved. Do you wish to leave the page?";
-        }
-    }
-    $("input[name='commit']").click(function() {
-        formmodified = 0;
-    });
 
     $('a.candidate_link').click(function( eventObject ){
         var elem = $( this );
@@ -20,5 +20,11 @@ $(document).ready(function() {
         console.log(elem.attr( "href" ));
     });
 
-
+    $('#jumbotron').on("change", ":input", function(){
+        c = $(this);
+        $.when(
+        c.focusout()).then(function() {
+        alert("changed " + c.attr("name"));
+	});
+    });
 });
